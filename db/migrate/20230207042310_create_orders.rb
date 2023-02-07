4d538c841fb29e74c08e07aba7a9e3f89845bbf4
keita.takaue <keita.takaue@gmail.com>
2023-02-07 13:53:06 +0900

    migrationとmodelの追加

--- db/migrate/20230207042310_create_orders.rb
+++ db/migrate/20230207042310_create_orders.rb
@@ -1,11 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-            t.references :food, null: false, foreign_key: true
-      t.references :restaurant, null: false, foreign_key: true
-      t.references :order, foreign_key: true
-      t.integer :count, null: false, default: 0
-      t.boolean :active, null: false, default: false
+      t.integer :total_price, null: false, default: 0
 
       t.timestamps
     end
